9671531434eb42e2e9287f5ea14d13d8236ce6e9
arcyro <arek@arcyro.pl>
2025-04-06 16:19:44 +0200

    db config

--- src/main/java/pl/merito/demo/BookController.java
+++ src/main/java/pl/merito/demo/BookController.java
@@ -3,6 +3,7 @@ package pl.merito.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Scanner;
@@ -25,5 +26,18 @@ public class BookController {
         model.addAttribute("books",
                 bookRepository.findAll());
         return "book/list";
+    }
+
+
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "book/add";
+    }
+
+    @PostMapping("/add")
+    public String addBookPost(Book book) {
+        bookRepository.save(book);
+        return "redirect:/books/list";
     }
 }
